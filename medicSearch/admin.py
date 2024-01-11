3d6486c3d5284cd70b7ae81eb94598e995df5224
Luciano Gouveia <ljdgb2012@gmail.com.br>
2024-01-11 12:52:59 -0300

    registra novas models no admin

--- medicSearch/admin.py
+++ medicSearch/admin.py
@@ -7,3 +7,10 @@ class ProfileAdmin(admin.ModelAdmin):
     
 
 admin.site.register(Profile, ProfileAdmin)
+admin.site.register(State)
+admin.site.register(City)
+admin.site.register(Neighborhood)
+admin.site.register(Address)
+admin.site.register(DayWeek)
+admin.site.register(Rating)
+admin.site.register(Speciality)
