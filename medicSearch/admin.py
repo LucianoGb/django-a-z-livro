3c568e44c4ab4d09ea7261633f81d96b80342bc0
Luciano Gouveia <ljdgb2012@gmail.com.br>
2024-01-11 11:45:56 -0300

    ajusta admin do projeto criando o profileadmin para ajustar a visualização

--- medicSearch/admin.py
+++ medicSearch/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role', 'birthday', 'created_at', 'updated_at', 'token')
+    
 
-admin.site.register(Profile)
+admin.site.register(Profile, ProfileAdmin)
