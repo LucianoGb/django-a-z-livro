48cfcef7f69bebd76e82c1642a68d0b96c9362a0
Luciano Gouveia <ljdgb2012@gmail.com.br>
2024-01-11 12:56:05 -0300

    adiciona upload de imagens

--- medicSearch/models/Profile.py
+++ medicSearch/models/Profile.py
@@ -6,9 +6,12 @@ class Profile(models.Model):
     birthday = models.DateField(default=None, null=True,blank=True)
     created_at	=	models.DateTimeField(auto_now_add=True)
     updated_at	=	models.DateTimeField(auto_now=True)
-    token	=	models.CharField(max_length=255,	null=True,	blank=True
-)
-    
+    token	=	models.CharField(max_length=255,	null=True,	blank=True)
+    image = models.ImageField(null = True, blank = True)
+    favorites = models.ManyToManyField(User, blank=True, related_name='favorites')
+    specialties = models.ManyToManyField(Speciality, blank=True,related_name='specialties')
+    addresses = models.ManyToManyField(Address, blank=True, related_name='addresses')
+        
 def	__str__(self):
     # return '{}'.format(self.user.first_name)  
     if self.user:
@@ -29,3 +32,5 @@ def	save_user_profile(sender,	instance,	**kwargs):
         instance.profile.save()
     except:
         pass
+    
+    
